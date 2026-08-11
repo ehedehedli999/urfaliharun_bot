19d63b83454ce9b338d1221d127bd6add71867f0
ehedehedli999 <146145398+ehedehedli999@users.noreply.github.com>
2026-08-12 03:27:02 +0400

    Change CEREBRAS_MODEL to llama3.1-70b
    Updated CEREBRAS_MODEL to use llama3.1-70b.

--- main.py
+++ main.py
@@ -12,12 +12,11 @@ from telegram.ext import (
     ContextTypes,
 )
 
-# Yeni Telegram ve Cerebras Tokenlerin eklendi
 TELEGRAM_TOKEN = "8363449973:AAElwMlaNrlKJ7sh8PApYPxWb13YqrHJakU"
 CEREBRAS_API_KEY = "csk-2nr3xkmt8x9eyfkc9hhc2nyrwf5nrx8kdt4pn8hwdjvewfxv"
 
 CEREBRAS_URL = "https://api.cerebras.ai/v1/chat/completions"
-CEREBRAS_MODEL = "llama3.1-8b"
+CEREBRAS_MODEL = "llama3.1-70b"
 
 USER_SCORES = {}
 LANG_STATUS = {"tr": True, "ru": True, "de": True}
